fc6224bf52e49fbbaa8e8575ffe7c52d3b9c5db0
Theodore Vasiloudis <tvas@sics.se>
2018-01-24 11:42:03 +0100

    Add second parameter sweep level, parallelize over all.

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 """
-Simple python script to sweep over a single parameter
+Simple python script to sweep over one or two parameters for our experiments.
 """
 import argparse
 from subprocess import run
 from pathlib import Path
 
 import numpy as np
-
-# TODO: Allow multi-parameter sweeps
+from joblib import Parallel, delayed
 
 
 def parse_args():
@@ -28,29 +27,68 @@ def parse_args():
                        help="A string representation of an argument range, enter"
                             "\"start end step\" as in np.arange(start, end, step)")
 
+    parser.add_argument("--njobs", type=int, default=1,
+                        help="Number of experiment jobs to run in parallel, max one per input file")
+
+    parser.add_argument("--verbose", type=int, default=0,
+                        help="Set to 1 output per experiment, 2 to write MOA output to stdout.")
+    parser.add_argument("--inner-sweep-argument",
+                        help="The argument to sweep on")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--inner-argument-list", nargs='+',
+                       help="A list of arguments to run")
+    group.add_argument("--inner-argument-range", nargs=3,
+                       help="A string representation of an argument range, enter"
+                            "\"start end step\" as in np.arange(start, end, step)")
+
     return parser.parse_args()
+
+
+def run_print(cur_command):
+    print("Running command:\n{}".format(cur_command))
+    run(cur_command, shell=True)
 
 
 def main():
     args = parse_args()
 
-    if args.argument_list is not None:
-        sweep = args.argument_list
-    else:
-        # We use this trick to correctly parse ints or float from string, so
-        # that the meta-args end up with the correct type
-        from ast import literal_eval as le
-        range_args = [le(x) for x in args.argument_range]
-        start, end, step = range_args
-        sweep = np.arange(start, end, step)
+    def parse_sweep(argument_list, argument_range):
+        if argument_list is not None:
+            sweep = argument_list
+        else:
+            # We use this trick to correctly parse ints or float from string, so
+            # that the meta-args end up with the correct type
+            from ast import literal_eval as le
+            range_args = [le(x) for x in argument_range]
+            start, end, step = range_args
+            sweep = np.arange(start, end, step)
+        return sweep
 
-    for value in sweep:
-        value = str(value)
-        outdir = Path(args.output_prefix) / value
-        command = args.command + " --{arg} {val} --output {outdir} ".format(
-            arg=args.sweep_argument, val=value, outdir=outdir)
-        print("Running command:\n{}".format(command))
-        run(command, shell=True)
+    outer_sweep = parse_sweep(args.argument_list, args.argument_range)
+    inner_sweep = None
+    if args.inner_argument_list is not None or args.inner_argument_range is not None:
+        inner_sweep = parse_sweep(args.inner_argument_list, args.inner_argument_range)
+
+    command_list = []
+    for outer_value in outer_sweep:
+        outer_value = str(outer_value)
+        outdir = Path(args.output_prefix) / outer_value
+        if inner_sweep is not None:
+            for inner_value in inner_sweep:
+                inner_value = str(inner_value)
+                inner_ouput = outdir / inner_value
+                command = args.command + " --{outer_arg} {outer_val} --{inner_arg} {inner_val}  --output {outdir} ".format(
+                    outer_arg=args.sweep_argument, outer_val=outer_value, outdir=inner_ouput,
+                    inner_arg=args.inner_sweep_argument, inner_val=inner_value)
+                command_list.append(command)
+        else:
+            command = args.command + " --{arg} {val} --output {outdir} ".format(
+                arg=args.sweep_argument, val=outer_value, outdir=outdir)
+            command_list.append(command)
+
+    with Parallel(n_jobs=args.njobs, verbose=args.verbose) as parallel:
+        parallel(delayed(run_print)(command)
+                 for command in command_list)
 
 
 if __name__ == "__main__":
